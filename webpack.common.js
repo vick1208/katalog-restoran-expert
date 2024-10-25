3747a87b1bea3977221f4a9c48dae61623ede987
Vicky AS <vickyhuang808@gmail.com>
2024-10-25 20:28:13 +0700

    edit webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const devMode = process.env.NODE_ENV !== "production";
+
 
 module.exports = {
   entry: {
