e20646c433773e1f6fea276f8847279db0863883
Vicky AS <vickyhuang808@gmail.com>
2024-11-07 19:04:26 +0700

    update scss and button svg

--- src/scripts/components/nav-bar.js
+++ src/scripts/components/nav-bar.js
@@ -12,11 +12,19 @@ class NavBar extends HTMLElement{
 
       <div class="hamburger">
         <button id="hamburger" aria-label="menu">
-          <svg class="hb" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4 18L20 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-            <path d="M4 12L20 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-            <path d="M4 6L20 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-          </svg>
+          <svg class="hb" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" stroke="currentColor" stroke-width=".6" fill="rgba(0,0,0,0)" stroke-linecap="round" style="cursor: pointer">
+  <path d="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7">
+    <animate dur="0.2s" attributeName="d" values="M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7;M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7" fill="freeze" begin="start.begin" />
+    <animate dur="0.2s" attributeName="d" values="M3,3L5,5L7,3M5,5L5,5M3,7L5,5L7,7;M2,3L5,3L8,3M2,5L8,5M2,7L5,7L8,7" fill="freeze" begin="reverse.begin" />
+  </path>
+  <rect width="10" height="10" stroke="none">
+    <animate dur="2s" id="reverse" attributeName="width" begin="click" />
+  </rect>
+  <rect width="10" height="10" stroke="none">
+    <animate dur="0.001s" id="start" attributeName="width" values="10;0" fill="freeze" begin="click" />
+    <animate dur="0.001s" attributeName="width" values="0;10" fill="freeze" begin="reverse.begin" />
+  </rect>
+</svg>
 
         </button>
       </div>
