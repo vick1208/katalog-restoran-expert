e83dd7a6527cc3858627746475fdcee54541d3fe
Vicky AS <vickyhuang808@gmail.com>
2024-11-15 13:15:40 +0700

    edit config

--- webpack.common.js
+++ webpack.common.js
@@ -2,10 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
 module.exports = {
   entry: {
-    app: path.resolve(__dirname,'src/scripts/index.js'),
+    app: path.resolve(__dirname, 'src/scripts/index.js'),
   },
   output: {
     filename: '[name].bundle.js',
@@ -35,8 +34,8 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
-          globOptions:{
-            ignore:['**/images/heros/hero-image_1.jpg'],
+          globOptions: {
+            ignore: ['**/images/heros/hero-image_1.jpg'],
           }
         },
       ],
